518a835e532d147b3ab1fc843efe3cd9a8b3f796
Vain-soft <genz123@yandex.ru>
2019-08-28 10:48:26 +0300

    0

--- git/Form1.Designer.cs
+++ git/Form1.Designer.cs
@@ -34,7 +34,7 @@
             // comboBox1
             // 
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(330, 100);
+            this.comboBox1.Location = new System.Drawing.Point(534, 56);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(121, 21);
             this.comboBox1.TabIndex = 0;
