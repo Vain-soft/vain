41ab8d43cede4b2f62af06bf370054ad5881d685
Булгаков Артем Николаевич <bulgakov.tema@mail.ru>
2019-08-28 10:58:19 +0300

    d

--- git/Form1.Designer.cs
+++ git/Form1.Designer.cs
@@ -44,11 +44,11 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label1.Location = new System.Drawing.Point(223, 131);
+            this.label1.Location = new System.Drawing.Point(238, 115);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(261, 73);
+            this.label1.Size = new System.Drawing.Size(120, 73);
             this.label1.TabIndex = 1;
-            this.label1.Text = "ATREM";
+            this.label1.Text = "YA";
             // 
             // Form1
             // 
